8b631510f10a4cdc04ce476f51b10a0e59007d19
Billy B <bilegbaatar@lewisu.edu>
2021-04-13 06:10:02 -0500

    new trial

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const sql = require('mssql');
+const path = require('path');
+
+const config ={
+
+    user: 'scrumples',
+    password: 'Utrade1234',
+    server: 'utrade.database.windows.net',
+    database: 'utrade-scrumples',
+    "options": {
+        "encrypt": true,
+        "enableArithAbort": true
+    }
+};
+
+sql.connect(config);
+const request = new sql.Request();
+
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory)); 
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+app.set('view engine', 'hbs');
+
+//Define Routes
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'))
+app.listen(5001, () => {
+    console.log("Server started on port 5001");
+
+ 
+     
+}
+
+)
---
+++ controllers/auth.js
@@ -0,0 +1,37 @@
+const sql = require('mssql');
+
+
+const config ={
+
+    user: 'scrumples',
+    password: 'Utrade1234',
+    server: 'utrade.database.windows.net',
+    database: 'utrade-scrumples',
+    "options": {
+        "encrypt": true,
+        "enableArithAbort": true
+    }
+};
+
+sql.connect(config);
+const request = new sql.Request();
+
+
+    
+
+
+
+
+exports.register = (req, res)=>{
+    console.log(req.body);
+
+ 
+
+
+    const {name, email, password, passwordConfirm} = req.body;
+
+    
+
+
+    res.send("Form submitted");
+}

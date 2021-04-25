2726cdd19a3a417794aba8fc69b1827f329b1de0
Tony Rispoli <anthonyrrispoli@lewisu.edu>
2021-04-25 17:59:26 -0500

    Added log in, log out, and sign up functionality to the site.

--- index.js
+++ index.js
@@ -1,26 +1,102 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
+var express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    flash = require('connect-flash'),
+    User = require("./models/users");
 
-router.get('/', function(req,res)  {
-    res.sendFile(path.join(__dirname+'/htmls/home.html'));
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb+srv://tony:tony123@cluster0.od1pi.mongodb.net/test?retryWrites=true&w=majority");
 
+var app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use(require("express-session")({
+    secret: "uTrade was created by the Scrumples",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(express.static("views"));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Routes
+
+app.get("/", function (req, res) {
+    res.render("home");
+})
+
+app.get("/home", function (req, res) {
+    res.render("home");
+})
+
+app.get("/loggedin", isLoggedIn, function (req, res) {
+    res.render("loggedin");
+})
+
+app.get("/register", function (req, res) {
+    res.render("register");
+})
+
+app.post("/register", function (req, res) {
+    var username = req.body.username
+    var password = req.body.password
+    User.register(new User({username: username}),
+            password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+
+        passport.authenticate("local")(
+            req, res, function () {
+                res.render("loggedin");
+        });
+    });
 });
-router.get('/about', function(req,res)  {
-    res.sendFile(path.join(__dirname+'/htmls/about.html'));
 
-});
-router.get('/contact', function(req,res)  {
-    res.sendFile(path.join(__dirname+'/htmls/contact.html'));
-
+app.get("/login", function (req, res) {
+    res.render("login");
 });
 
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/loggedin",
+    failureFlash: "Invalid username or password."
+}), function (req, res) {
+});
 
-app.use('/', router);
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+});
 
+app.get("/about", function (req, res) {
+    res.render("about");
+});
 
+app.get("/contact", function (req, res) {
+    res.render("contact");
+});
 
-app.listen(5000, () => {
-    console.log('App listening on port 5000');
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/login");
+}
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("Server has started!");
 });
--- models/users.js
+++ models/users.js
@@ -1,52 +1,10 @@
-const { Connection, Request } = require("tedious");
+var mongoose=require("mongoose");
+var passportlocalmongoose=require("passport-local-mongoose");
 
-// Create connection to database
-const config = {
-  authentication: {
-    options: {
-      userName: "scrumples", // update me
-      password: "Utrade1234" // update me
-    },
-    type: "default"
-  },
-  server: "utrade.database.windows.net", // update me
-  options: {
-    database: "utrade-scrumples", //update me
-    encrypt: true
-  }
-};
-
-const connection = new Connection(config);
-
-// Attempt to connect and execute queries if connection goes through
-connection.on("connect", err => {
-  if (err) {
-    console.error(err.message);
-  } else {
-    queryDatabase();
-  }
+var UserSchema=mongoose.Schema({
+  username: String,
+  password: String
 });
 
-function queryDatabase() {
-  console.log("Reading rows from the Table...");
-
-  // Read all rows from table
-  const request = new Request(
-    
-    (err, rowCount) => {
-      if (err) {
-        console.error(err.message);
-      } else {
-        console.log(`${rowCount} row(s) returned`);
-      }
-    }
-  );
-
-  request.on("row", columns => {
-    columns.forEach(column => {
-      console.log("%s\t%s", column.metadata.colName, column.value);
-    });
-  });
-
-  connection.execSql(request);
-}
+UserSchema.plugin(passportlocalmongoose);
+module.exports=mongoose.model("User", UserSchema);
